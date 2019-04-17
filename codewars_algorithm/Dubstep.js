f133322353cffa693d1a93cec0dc1da146bab9b6
Woochul Hyun <woochul.hyun91@gmail.com>
2019-04-17 19:08:09 +0900

    finish

---
+++ codewars_algorithm/Dubstep.js
@@ -0,0 +1,24 @@
+var song = "WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB";
+
+function songDecoder(song) {
+    var songArr = song.split("");
+    console.log(songArr);
+    for (var i = 0; i < songArr.length; i++) {
+        if (songArr[i] === "W") {
+            if (songArr[i + 1] === "U" && songArr[i + 2] === "B") {
+                songArr.splice(i, 3, " ");
+                i = i - 3;
+            }
+        }
+    }
+    for (var j = 0; j < songArr.length; j++) {
+        if (songArr[j] === " " && songArr[j + 1] === " ") {
+            songArr.splice(j, 1);
+            j--;
+        }
+    }
+    console.log(songArr);
+    return songArr.join("").trim();
+}
+
+console.log(songDecoder(song));
