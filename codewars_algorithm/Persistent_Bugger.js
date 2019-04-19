3844497291b4f1d97699a6c0a8480241dc2c020f
Woochul Hyun <woochul.hyun91@gmail.com>
2019-04-19 16:11:09 +0900

    finish

--- codewars_algorithm/Persistent_Bugger.js
+++ codewars_algorithm/Persistent_Bugger.js
@@ -1,22 +1,3 @@
-// var num = 4;
-
-// function persistence(num) {
-//     var i = 0;
-//     while (num >= 10) {
-//         var strNum = String(num);
-//         var splitedNum = strNum.split("");
-//         splitedNum.reduce(function(prev, value) {
-//             num = Number(prev) * Number(value);
-//             return num;
-//         });
-//         i++;
-//     }
-
-//     return i;
-// }
-
-// console.log(persistence(num));
-
 var num = 999;
 
 function persistence(num) {
