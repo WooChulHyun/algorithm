731268260405434d59d0458c607ce47cfb86d08a
Woochul Hyun <woochul.hyun91@gmail.com>
2019-09-26 16:10:31 +0900

    finish

--- programmers/완주하지 못한 선수.js
+++ programmers/완주하지 못한 선수.js
@@ -1,12 +1,19 @@
 function solution(participant, completion) {
   let answer = '';
+  let order = 0;
   const sortParticipant = participant.sort();
   const sortCompletion = completion.sort();
 
-  answer = sortParticipant.filter(
-    (name, index) => name !== sortCompletion[index]
-  );
-  return answer[0];
+  while (order < sortParticipant.length) {
+    if (sortParticipant[order] !== sortCompletion[order]) {
+      answer = sortParticipant[order];
+      break;
+    } else {
+      order++;
+    }
+  }
+
+  return answer;
 }
 
 console.log(solution(['leo', 'kiki', 'eden'], ['eden', 'kiki']));
